360c72a5a3514a0aa805894ea3bfaa3033575aea
shivendradixit9120 <shivendradixit9120@gmail.com>
2026-06-13 11:01:31 +0530

    Day17

---
+++ UnionArray.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+public class UnionArray {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {3, 4, 5, 6};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr1) {
+            set.add(num);
+        }
+
+        for (int num : arr2) {
+            set.add(num);
+        }
+
+        System.out.println("Union: " + set);
+    }
+}
