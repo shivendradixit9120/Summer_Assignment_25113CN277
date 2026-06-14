d7f8c1e4f4cc333755712e2a5e9fff30719cdac9
shivendradixit9120 <shivendradixit9120@gmail.com>
2026-06-14 20:30:16 +0530

    Day18

---
+++ 70.java
@@ -0,0 +1,24 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 9};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        System.out.print("Sorted Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
