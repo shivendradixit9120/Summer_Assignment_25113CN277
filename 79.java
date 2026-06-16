22c4be6c3285a082cef7b676683be4ac09e3dd52
shivendradixit9120 <shivendradixit9120@gmail.com>
2026-06-16 19:59:50 +0530

    Day20

---
+++ 79.java
@@ -0,0 +1,25 @@
+public class SymmetricMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {2, 4, 5},
+            {3, 5, 6}
+        };
+
+        boolean symmetric = true;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    symmetric = false;
+                    break;
+                }
+            }
+        }
+
+        if (symmetric)
+            System.out.println("Symmetric Matrix");
+        else
+            System.out.println("Not a Symmetric Matrix");
+    }
+}
---
+++ 80.java
@@ -0,0 +1,19 @@
+public class ColumnWiseSum {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        for (int j = 0; j < matrix[0].length; j++) {
+            int sum = 0;
+
+            for (int i = 0; i < matrix.length; i++) {
+                sum += matrix[i][j];
+            }
+
+            System.out.println("Sum of Column " + (j + 1) + " = " + sum);
+        }
+    }
+}
